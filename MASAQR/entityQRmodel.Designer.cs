308bb2c6f4743d91040e35e8a01febee89ee7f25
Mfundo Dhladhla <mfundod@contour.co.za>
2022-02-07 20:18:35 +0200

    Changes

--- MASAQR/entityQRmodel.Designer.cs
+++ MASAQR/entityQRmodel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\mdhla\source\repos\MASAQR\MASAQR\entityQRmodel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\MfundoD\Source\Repos\LMGside\MASAQR\MASAQR\entityQRmodel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
